ccb674c4063a1c22c2c350518734bd21ddc458b3
Sebastian <a20206438@pucp.edu.pe>
2023-08-24 21:01:24 -0500

    akaka

---
+++ 20206438-LAB1-GTICS/lab_gtics/src/main/java/com/example/lab_gtics/LabGticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabGticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabGticsApplication.class, args);
+    }
+
+}
---
+++ 20206438-LAB1-GTICS/lab_gtics/src/test/java/com/example/lab_gtics/LabGticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabGticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
